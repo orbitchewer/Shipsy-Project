3648c70ab32b978e3eb532eb7fb4e7f843acba27
orbitchewer <kanishka.sahay.01@gmail.com>
2025-10-05 16:01:41 +0530

    change

--- shipsyapp/src/components/PrivateRoute.jsx
+++ shipsyapp/src/components/PrivateRoute.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
-const PrivateRoute = ({children}) => {
-  return localStorage.getItem("valid") ? children : <Navigate to="/" />
+export default function PrivateRoute({ children, allowedRole }) {
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/verify", { withCredentials: true })
+      .then((res) => {
+        if (res.data.Status && res.data.role === allowedRole) {
+          setAuth(true);
+        } else {
+          setAuth(false);
+        }
+      })
+      .catch(() => setAuth(false));
+  }, [allowedRole]);
+
+  if (auth === null) return <div>Loading...</div>;
+
+  return auth ? children : <Navigate to="/" />;
 }
-
-export default PrivateRoute
